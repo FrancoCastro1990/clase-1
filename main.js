99d3e4f973cf3c52699303ca15df94e8c6210f06
Franco Castro <francoaggro90@gmail.com>
2018-05-23 13:39:43 -0400

    clase 1

--- main.js
+++ main.js
@@ -113,23 +113,112 @@ var nombre = (params) => {
 	//bloque de codigo
 }
 */
-
-function nombreCompleto(nombre,apellido){
-	
-	console.log(nombre+" "+apellido);
-	
-}
-nombreCompleto("Franco","Castro");
-
+/*
 function edad(anioNac){
 	
 	return 2018-anioNac;
 	
 }
 
+function validadEdad(num0){
 
-if(edad(2017)>=18){
-	console.log("eres mayor de edad");
-}else{
-	console.log("eres menor de edad");
+	if(edad(num0)>=18){
+		console.log("eres mayor de edad");
+	}else{
+		console.log("eres menor de edad");
+	}
+}
+var nombre1;
+var anioedad;
+nombre1=prompt("hola!, ingrese nu nombre por favor","karina",);
+anioedad=prompt("ingrese su año de nacimiento","1990");
+console.log("hola "+nombre1+", tienes", edad(anioedad),"años");
+validadEdad(anioedad);
+
+var x=1;
+  var suma=0;
+  var valor;
+  while (x<=5)
+  {
+    valor=prompt('Ingrese valor:','');
+    valor=parseInt(valor);
+    suma=suma+valor;
+    x=x+1;
+  }
+  document.write('La suma de los valores es '+suma+'<br>');
+  */
+ /*
+ class Poligono {
+	constructor(height, width) {
+	  this.height = height;
+	  this.width = width;
+	}
+	
+	get area() {
+	  return this.calcArea();
+	}
+  
+	calcArea() {
+	  return this.height * this.width;
+	}
+  }
+  
+  var cuadrado = new Poligono(10, 10);
+  
+  console.log(cuadrado.area);
+  console.log(cuadrado.height);
+  console.log(cuadrado.width);
+  */
+ 
+var numJugadores=0;
+var nombre=" ";
+var contador=0;
+var terminar=false;
+var seleccion1=0, seleccion2=0;
+var puntos1=0;
+
+numJugadores=prompt("Ingrese cantidad de jugadores: ");
+var jugador=new Array(numJugadores);
+console.log(numJugadores);
+
+for (let i = 0; i < numJugadores; i++) {
+	jugador[i]=new Array(4);
+}
+//0 nombre, 1 ID, 2 Npartidas,3 Puntos
+
+do{
+	nombre=prompt("ingrese el nombre del jugador:");
+	jugador[contador][0]=nombre;//nombre
+	jugador[contador][1]=contador+1;//ID
+	jugador[contador][2]=numJugadores-1;//CANTIDAD DE PARTIDAS POR JUGAR
+	jugador[contador][3]=0;//PUNTOS
+	contador++;
+}while(contador<numJugadores);
+
+while(!terminar){
+	for (let j = 0; j < numJugadores; j++) {
+			console.log("ID:",jugador[j][1],"Nombre;",jugador[j][0],",P. POR JUGAR:",jugador[j][2],",PUNTOS:",jugador[j][3]);
+	}
+	for (let x = 0; x < 2; x++) {
+		seleccion1=seleccion2;
+		seleccion2=prompt("ingrese la ID del jugador.");
+	}
+
+	for(let z = 0; z < numJugadores; z++){
+		if(jugador[z][1]==seleccion1 && jugador[z][2]!=0){
+			puntos1=prompt("ingrese los puntos del jugador 1");
+			jugador[z][3]=+puntos1;
+			jugador[z][2]--;		
+		}
+		if(jugador[z][1]==seleccion2 && jugador[z][2]!=0){
+			puntos1=prompt("ingrese los puntos del jugador 2");
+			jugador[z][3]=+puntos1;
+			jugador[z][2]--;
+		}
+		
+	}
+
+
+	
+
 }
