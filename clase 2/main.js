4f2f62857f8fd05356060fabcea1af5a851b0fb6
Franco <francoaggro90@gmail.com>
2018-05-26 11:37:59 -0400

    funciones con arreglos

--- clase 2/main.js
+++ clase 2/main.js
@@ -14,14 +14,38 @@ console.log(saludar("franco"));
 console.log("funcion en clase 2");
 */
 
+
 var valor = "global";
 
 function funcionLocal() {
-	var valor = "local";
+	valor = "local";
 	return valor;
 }
+
+
 
 console.log(valor);
 console.log(funcionLocal());
 console.log(valor);
+
+var miArray=new Array();
+
+for (let i = 0; i < 8; i++) {
+	miArray[i]=i+1;
+}
+var miArray2 = [4,1,3,2];
+console.log("valor en arreglo",miArray2);
+miArray2.sort();
+console.log("valor en arreglo ",miArray2);
+miArray2.push(5);
+console.log("el valor agregado al final es ",miArray2);
+
+function add(num) {
+	miArray2.push(num);
+}
+add(10);
+console.log(miArray2);
+
+
+
 
