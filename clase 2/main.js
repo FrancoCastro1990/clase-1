07ee44c56e1efb3d82ea65dfc24485460de07887
Franco <francoaggro90@gmail.com>
2018-05-26 11:09:29 -0400

    clase 2

--- clase 2/main.js
+++ clase 2/main.js
@@ -1,5 +1,5 @@
 
-function prueba(numero1) {
+/*function prueba(numero1) {
 	return numero1;
 }
 console.log(prueba(1));
@@ -9,16 +9,19 @@ function saludar(nombre) {
 }
 console.log(saludar("franco"));
 
+
+
+console.log("funcion en clase 2");
+*/
+
 var valor = "global";
+
 function funcionLocal() {
 	var valor = "local";
 	return valor;
 }
+
 console.log(valor);
 console.log(funcionLocal());
 console.log(valor);
-console.log("funcion en clase 2");
-
-
-
 
