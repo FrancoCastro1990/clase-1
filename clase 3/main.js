b8e9aef1a35beaea132c4c8e3129a341a2580fa8
Franco <francoaggro90@gmail.com>
2018-06-02 11:02:05 -0400

    ok

--- clase 3/main.js
+++ clase 3/main.js
@@ -17,20 +17,18 @@ var auto = {
 	frenar: function () {
 		return 'el auto se freno';
 	},
-	encender: function(){
-		this.encendido = true;
+	encender: function(val){
+		this.encendido = val;
 		//el this es igual que auto.encendido
 	},
-	apagar: function(){
-		this.encendido = false;
-	},
+
 }
 console.log(auto);
 console.log(auto.marca);
 console.log(auto.encendido);//verificamos el el auto esta encendido
-auto.encender();//encendimos el auto
+auto.encender(true);//encendimos el auto
 console.log(auto.encendido);
-auto.apagar();
+auto.encender(false);
 console.log(auto.encendido);
 
 
