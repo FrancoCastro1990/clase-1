b0e6ab017854613567c76794b9f2cdb9468aedbc
Franco <francoaggro90@gmail.com>
2018-06-23 13:56:37 -0400

    clase 6

--- clase 6/dom.js
+++ clase 6/dom.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded',function () {
+document.addEventListener('DOMContentLoaded', function () {
     var tags = document.getElementsByTagName('input');
     //console.log(tags);
     //console.log("tag 2 ->",tags[2]);
@@ -22,11 +22,56 @@ document.addEventListener('DOMContentLoaded',function () {
     /*console.log(clase.classList);
     clase.classList.add('segundaClase');
     console.log(clase.classList);*/
-    var clase1 =document.querySelector(".clase1");
-    //var btn3 =document.querySelector("#btn3");
-    var btn3 =document.getElementById('btn3');
 
-    btn3.addEventListener('click',function(){
+    //guardamos en clase1 
+    var clase1 = document.querySelector(".clase1");
+    //var btn3 =document.querySelector("#btn3");
+    var btn3 = document.getElementById('btn3');
+    var btn4 = document.getElementById('btn4');
+    var btn5 = document.getElementById('btn5');
+    var btn6 = document.getElementById('btn6');
+    var btn7 = document.getElementById('btn7');
+    var btn8 = document.getElementById('btn8');
+
+    btn3.addEventListener('click', function () {
+        //add agrega una clase
         clase1.classList.add('clase2');
     })
+
+    btn4.addEventListener('click', function () {
+        //si la clase existe la elimina, si no existe la agrega
+        clase1.classList.toggle('clase2');
+    })
+
+    btn5.addEventListener('click', function () {
+        //elimina una clase
+        clase1.classList.remove('clase2');
+    })
+
+    btn6.addEventListener('click', function () {
+        //devuelve true si existe
+        if (clase1.classList.contains('clase2')) {
+            alert("existe la clase")
+        } else {
+            alert("no existe la clase");
+        }
+    })
+
+    btn7.addEventListener('click', function () {
+        //remplasa una clase
+        clase1.classList.replace('clase1', 'clase2');
+    })
+
+
+    btn8.addEventListener('click', function () {
+        btn8.innerText = "cambiamos";
+        document.body.innerHTML += `
+            <header>
+                <p class="clase1">agregamos una linea</p>
+            </header>
+        `;
+        
+    })
+    //btn8.innerText = "cambiamos";
+
 })
