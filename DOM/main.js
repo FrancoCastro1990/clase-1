75d93d78844134ef3b9247e6cc7edae32d86c4da
francoaggro90 <francoaggro90@gmail.com>
2018-07-03 21:13:43 -0400

    dom

--- DOM/main.js
+++ DOM/main.js
@@ -2,9 +2,12 @@ console.log("te amo");
 
 var lista = document.getElementById("lista");
 
-for(let i=0;i<10;i++){
+for(let i=0;i<5;i++){
     lista.innerHTML+=`
     <li>
         <p>hola ${i}</p>
+        <p>hola ${i+1}</p>
+        <p>hola ${i+2}</p>
+        <p>hola ${i+3}</p>
     </li>`;
 }
